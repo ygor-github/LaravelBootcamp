6ecccfd65fa25242c1bf62549e12f82a588d1155
ygor-github <ygorhernandezbr@gmail.com>
2024-12-01 15:26:56 -0300

    Creación de Modelo, Controllador y Vistas de Chirp, Ajustes de Link de navegación, permisos de escritura en la base de datos

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
         //
+        return view('chirps.index');
     }
 
     /**
@@ -26,9 +29,15 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+        public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $request->user()->chirps()->create($validated);
+ 
+        return redirect(route('chirps.index'));
     }
 
     /**
--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Chirp extends Model
 {
     //
+    protected $fillable = [
+        'message',
+    ];
 }
