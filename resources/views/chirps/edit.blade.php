70d50db79b1cc9ff6e12c60648e4443c190735df
ygor-github <ygorhernandezbr@gmail.com>
2024-12-09 22:40:37 -0300

    Se crea el componente para el boton Danger y se actualiza en la vista editar Chirp

--- resources/views/chirps/edit.blade.php
+++ resources/views/chirps/edit.blade.php
@@ -10,7 +10,7 @@
             <x-input-error :messages="$errors->get('message')" class="mt-2" />
             <div class="mt-4 space-x-2">
                 <x-primary-button>{{ __('Save') }}</x-primary-button>
-                <a href="{{ route('chirps.index') }}">{{ __('Cancel') }}</a>
+                <a href="{{ route('chirps.index') }}"><x-danger-button>{{ __('Cancel') }}</x-danger-button></a>
             </div>
         </form>
     </div>
